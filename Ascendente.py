583ec16519d851feca6be6940b31d2929d89180a
Juan Daniel Carreño Rodriguez <126117081+JDaniec@users.noreply.github.com>
2023-03-09 10:35:49 -0500

    Add files via upload

---
+++ Ascendente.py
@@ -0,0 +1,16 @@
+print("------------------------------------------------")
+print("------Ordenar numeros de forma ascendente-------")
+print("---------------------------------------------")
+
+# intput
+a = int(input("Digite el primer numero "))
+b = int(input("Digite el valor del segundo numero "))
+c = int(input("Digite el valor del tercer numero "))
+
+# processsing and output
+
+if a > b and a > c :   
+    if b > c
+    print("El numero mayor es" + str(a) "el de la mitad" + str(b) "y el ultimo" + str(c))
+       
+       
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NUmeros-ordenados
 Elaborar un programa que dados 3 números los devuelva en orden ascendete
+!
