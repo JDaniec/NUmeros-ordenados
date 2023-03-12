64bd66891e9ad8f60eea14f490baf43af06ca73f
JDaniec <juan2234517@correo.uis.edu.co>
2023-03-11 20:06:37 -0500

    Subir cambios

--- Ascendente.py
+++ Ascendente.py
@@ -10,7 +10,25 @@ c = int(input("Digite el valor del tercer numero "))
 # processsing and output
 
 if a > b and a > c :   
-    if b > c
-    print("El numero mayor es" + str(a) "el de la mitad" + str(b) "y el ultimo" + str(c))
-       
+    if b > c:
+           print("El numero menor es " + str(c) + " el numero de la mitad es " + str(b) + " y el numero mayor es " + str(a))
+    if c > b:
+          print("El numero menor es " + str(b) + " el numero de la mitad es " + str(c) + " y el numero mayor es " + str(a))
+           
+
+if b > a and b > c :
+      if a > c:
+            print("El numero menor es " + str(c) + " el numero de la mitad es " + str(a) + " y el numero mayor es " +str(b))
+
+      if c > a:
+            print("El numero menor es " + str(a) + " el numero de la mitad es " + str(c) + " y el numero mayor es " +str(b))
+
+if c > a and c > b :
+      if a > b:
+            print("El numero menor es " + str(b) + " el numero de la mitad es " + str(a) + " y el numero mayor es " +str(c))
+
+      if b > a:
+            print("El numero menor es " + str(a) + " el numero de la mitad es " + str(b) + " y el numero mayor es " +str(c))
+
+      
        
